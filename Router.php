d756a47d8d9450943b87559f0b17b529a573899e
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-05-13 17:59:49 +0200

    added router class handling request routing

---
+++ Router.php
@@ -0,0 +1,82 @@
+<?php
+
+class Router
+{
+    protected $routes = [];
+
+    public function registerRoute($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'url' => $uri,
+            'controller' => $controller,
+        ];
+    }
+
+    /**
+     * Add a GET route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function get($uri, $controller)
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    /**
+     * Add a POST route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function post($uri, $controller)
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    /**
+     * Add a PUT route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function put($uri, $controller)
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+    /**
+     * Add a DELETE route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function delete($uri, $controller)
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    /**
+     * Route the request
+     * 
+     * @param string $uri
+     * @param string $method
+     * @return void
+     */
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['url'] === $uri && $route['method'] === $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+        http_response_code(404);
+        loadView('error/404');
+        exit;
+    }
+}
