6f45d8e53e64bc1850d940926a39a4b17682dfc5
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-05-17 21:49:00 +0200

    added fatched listings

--- Database.php
+++ Database.php
@@ -15,13 +15,33 @@ class Database
         $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
 
         $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
 
         try {
-            $this->conn = new PDO($dsn, $config['username'], $config['password']);
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
         } catch (PDOException $e) {
             throw new Exception("Database connection failed: . {$e->getMessage()}");
+        }
+    }
+
+    /**
+     * Query the database
+     * 
+     * @param string $query
+     * 
+     * @return PDOStatement
+     * @throws PDOexception
+     */
+    public function query($query)
+    {
+        try {
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        } catch (PDOException $e) {
+            throw new Exception("Query failed: {$e->getMessage()}");
         }
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,17 +1,17 @@
 <?php
 require '../helpers.php';
-//require basePath('Database.php');
-//$config = require basePath('config/db.php');
-
-//$db = new Database($config);
-
+require basePath('Database.php');
 require basePath('Router.php');
 
+//Initialize the router
 $router = new Router();
 
+//Get paths to router
 $routes = require basePath('routes.php');
 
+//Get current URI and HTPP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+//Route the request
 $router->route($uri, $method);
