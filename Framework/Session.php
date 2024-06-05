d5d8efc0965759e5ef51fa86b7eb7c6b72a50fda
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-06-05 15:38:41 +0200

    added methods to get key session and get that key

--- Framework/Session.php
+++ Framework/Session.php
@@ -15,4 +15,28 @@ class Session
             session_start();
         }
     }
+
+    /**
+     * Set a session key and value pair
+     * 
+     * @param string $keu
+     * @param mixed $value
+     * @return void
+     */
+    public static function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Get a session value by key
+     * 
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public static function get($key, $default = null)
+    {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : $default;
+    }
 }
