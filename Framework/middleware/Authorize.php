d1c150bb816f08febd7bda4ad36260737f71d38e
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-06-10 16:22:19 +0200

    added Authorize class and methods to authorize user status and redirects caused by status

---
+++ Framework/middleware/Authorize.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Framework\Middleware;
+
+use Framework\Session;
+
+class Authorize
+{
+    /**
+     * Check if user is authorized
+     * 
+     * @return bool
+     */
+    public function isAuthorized()
+    {
+        return Session::has('user') ? true : false;
+    }
+
+    /**
+     * Hanlde users request
+     * 
+     * @param string $role
+     * @return bool
+     */
+    public function handle($role)
+    {
+        if ($role === 'quest' && $this->isAuthorized()) {
+            return redirect('/');
+        } elseif ($role === 'auth' && !$this->isAuthorized()) {
+            return redirect('/auth/login');
+        }
+    }
+}
